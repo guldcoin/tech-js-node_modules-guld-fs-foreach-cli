57d9f59e5a526dccbca29f86b63bd0ffd053eba6
Ira Miller <public@iramiller.com>
2018-09-04 15:20:06 -0500

    runCLI

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 const foreach = require('guld-fs-foreach')
 const program = require('commander')
 const spawn = require('guld-spawn').getSpawn()
+const runCLI = require('guld-cli-run')
 const thispkg = require(`${__dirname}/package.json`)
 
 /* eslint-disable no-console */
@@ -9,10 +10,10 @@ program
   .name(thispkg.name.replace('-cli', ''))
   .version(thispkg.version)
   .description(thispkg.description)
-//  .description('Run command for each file and/or directory in the given directory.')
   .usage('<dir> <command> [flags...]', 'Run command for each file and/or directory in the given directory.')
   .action(async (dir, c, flags) => {
     var moreArgs
+    dir = dir || '.'
     if (flags && flags.rawArgs) moreArgs = flags.rawArgs.slice(4)
     else moreArgs = []
     return foreach(dir, async f => {
@@ -20,10 +21,7 @@ program
       return spawn(c, '', [f])
     }, moreArgs)
   })
+/* eslint-enable no-console */
 
-if (process.argv.length === 2) {
-  program.help()
-} else if (process.argv.length > 2) {
-  program.parse(process.argv)
-}
+runCLI.bind(program)()
 module.exports = program
